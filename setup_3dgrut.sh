b9ad23beb7b0b5de21c7282ed47f15300a416091
prerana <prerana.chaks@gmail.com>
2025-05-06 09:57:04 +0000

    environment setup for training

--- setup_3dgrut.sh
+++ setup_3dgrut.sh
@@ -1,4 +1,7 @@
-# sudo apt-get install gcc-11 g++-11
-# CUDA_VERSION=12.8.1 ./install_env.sh 3dgrut_cuda12 WITH_GCC11
+sudo apt-get install gcc-11 g++-11
+CUDA_VERSION=12.8.1 ./install_env.sh 3dgrut_cuda12 WITH_GCC11
 pip install kagglehub
+pip install torch
+pip install -e .
+pip install -r requirements.txt
 python nerf_data_download.py 
---
+++ train_3dgrut.sh
@@ -0,0 +1,5 @@
+# Train Lego with 3DGRT & 3DGUT
+
+python train.py --config-name apps/nerf_synthetic_3dgut.yaml path=/home/.cache/kagglehub/datasets/nguyenhung1903/nerf-synthetic-dataset/versions/1/nerf_synthetic/lego\
+ out_dir=runs experiment_name=lego_3dgut
+
